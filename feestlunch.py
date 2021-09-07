ce9eb4ac7a567c140f06ac11ca276405102cdabd
Bilal094 <99066318@mydavinci.nl>
2021-09-07 21:13:19 +0200

    Python programma's gemaakt en toegevoegd

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+AantalCroissants = 17
+PrijsCroissant = 0.39
+AantalStokbrood = 2
+PrijsStokbrood = 2.78
+KortingBon = 0.50
+AantalKortingBon = 3
+
+print((AantalCroissants * PrijsCroissant) + (AantalStokbrood * PrijsStokbrood) - (AantalKortingBon * KortingBon))
---
+++ speeldag.py
@@ -0,0 +1,6 @@
+ToegangPrijs = 7.45
+Personen = 4
+PrijsGameSeat = 0.37
+AantalVijfMinuten = 9
+
+print((ToegangPrijs * Personen) + (PrijsGameSeat * AantalVijfMinuten * Personen))
