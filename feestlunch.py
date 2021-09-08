175a28db541a908628073b04789b2c4fbd7f64cd
Bilal094 <99066318@mydavinci.nl>
2021-09-08 09:55:49 +0200

    prints gemaakt en toegevoegd en programmas afgemaakt

--- feestlunch.py
+++ feestlunch.py
@@ -4,5 +4,7 @@ AantalStokbrood = 2
 PrijsStokbrood = 2.78
 KortingBon = 0.50
 AantalKortingBon = 3
+PrijsFeestLunch = 10.69
 
 print((AantalCroissants * PrijsCroissant) + (AantalStokbrood * PrijsStokbrood) - (AantalKortingBon * KortingBon))
+print('De feestlunch kost je bij de bakker '+ str(PrijsFeestLunch) +' voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
