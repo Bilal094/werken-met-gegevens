ac453845fd51c206e704c87d34680f0cf5a6cb9c
Bilal094 <99066318@mydavinci.nl>
2022-01-19 11:34:29 +0100

    prijstoegang aangepast

--- speeldag.py
+++ speeldag.py
@@ -2,7 +2,7 @@ ToegangPrijs = 7.45
 Personen = 4
 PrijsGameSeat = 0.37
 AantalVijfMinuten = 9
-PrijsToegang = 43.12
 
-print((ToegangPrijs * Personen) + (PrijsGameSeat * AantalVijfMinuten * Personen))
+
+PrijsToegang = (ToegangPrijs * Personen) + (PrijsGameSeat * AantalVijfMinuten * Personen)
 print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar '+ str(PrijsToegang) +' euro.')
